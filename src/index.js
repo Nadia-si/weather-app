19baf1dbc79ab457391e6d2f5ca1407ec0290f40
Nadia-si <nasimbi2011@gmail.com>
2023-03-18 22:41:42 +0100

    Arrange codes and remove console log

--- src/index.js
+++ src/index.js
@@ -20,21 +20,22 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
   let humidityElelement = document.querySelector("#humidity");
-  humidityElelement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#windSpeed");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElelement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  console.log(response.data);
 }
 
 let apiKey = "1a8e14dead82d28c527cf46b1e514682";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=uppsala&appid=${apiKey}&units=metric`;
+let city = "Uppsala";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
