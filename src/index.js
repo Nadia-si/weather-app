38b3aeb7596260a2a19f5fb74577109b4b725589
Nadia-si <nasimbi2011@gmail.com>
2023-03-18 22:35:07 +0100

    Updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return ` ${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -9,6 +29,8 @@ function displayTemperature(response) {
   humidityElelement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#windSpeed");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   console.log(response.data);
 }
 
