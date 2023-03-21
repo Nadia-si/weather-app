0496e09a927b03f2c64478ebd0eb46cbcee6ea3d
Nadia-si <nasimbi2011@gmail.com>
2023-03-21 21:19:21 +0100

    Added forecast JS template

--- src/index.js
+++ src/index.js
@@ -18,6 +18,34 @@ function formatDate(timestamp) {
   return ` ${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat", "sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="https://openweathermap.org/img/wn/04n@2x.png"
+          alt=""
+          width="43"
+        />
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max"> 4°</span>
+          <span class="weather-forecast-temperature-min"> 2°</span>
+        </div>
+      </div> 
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -87,3 +115,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Uppsala");
+
+displayForecast();
